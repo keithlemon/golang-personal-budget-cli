2800e4696b5287e309f7d591f20f5998ae10d1fc
Keith Lemon <keithlemon@gmail.com>
2022-05-21 11:37:05 -0500

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,4 +2,18 @@ package module1
 
 // Budget stores budget information
 
+type Budge struct {
+	Name string
+	Max float32
+	Items []Item
+}
+
 // Item stores item information
+type Item struct {
+	// Name of the item
+	Name string
+	Description string
+	Price float32
+}
+
+
